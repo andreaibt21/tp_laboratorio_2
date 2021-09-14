535fb70cf04abcb99542ec4ce7c89c71acfa4651
andreaibt21 <andreaixabella@gmail.com>
2021-09-13 23:57:00 -0300

    Primer commit

---
+++ TP1/TP1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace TP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.cmbOperar = new System.Windows.Forms.ComboBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblOperaciones = new System.Windows.Forms.ListBox();
+            this.textNumero1 = new System.Windows.Forms.TextBox();
+            this.textNumero2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.BackColor = System.Drawing.Color.Teal;
+            this.btnOperar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOperar.Location = new System.Drawing.Point(62, 148);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(127, 39);
+            this.btnOperar.TabIndex = 0;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Teal;
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLimpiar.Location = new System.Drawing.Point(229, 148);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(127, 39);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.BackColor = System.Drawing.Color.Teal;
+            this.BtnCerrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnCerrar.Location = new System.Drawing.Point(405, 148);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(127, 39);
+            this.BtnCerrar.TabIndex = 2;
+            this.BtnCerrar.Text = "Cerrar";
+            this.BtnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // btnConvertirABinario
+            // 
+            this.btnConvertirABinario.BackColor = System.Drawing.Color.Teal;
+            this.btnConvertirABinario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConvertirABinario.Location = new System.Drawing.Point(92, 214);
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.Size = new System.Drawing.Size(148, 39);
+            this.btnConvertirABinario.TabIndex = 3;
+            this.btnConvertirABinario.Text = "Convertir a Binario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = false;
+            // 
+            // btnConvertirADecimal
+            // 
+            this.btnConvertirADecimal.BackColor = System.Drawing.Color.Teal;
+            this.btnConvertirADecimal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConvertirADecimal.Location = new System.Drawing.Point(295, 214);
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Size = new System.Drawing.Size(148, 39);
+            this.btnConvertirADecimal.TabIndex = 4;
+            this.btnConvertirADecimal.Text = "Convertir a Decimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = false;
+            // 
+            // cmbOperar
+            // 
+            this.cmbOperar.FormattingEnabled = true;
+            this.cmbOperar.Location = new System.Drawing.Point(243, 77);
+            this.cmbOperar.Name = "cmbOperar";
+            this.cmbOperar.Size = new System.Drawing.Size(75, 28);
+            this.cmbOperar.TabIndex = 5;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(498, 40);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(75, 20);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblOperaciones
+            // 
+            this.lblOperaciones.FormattingEnabled = true;
+            this.lblOperaciones.ItemHeight = 20;
+            this.lblOperaciones.Location = new System.Drawing.Point(586, 27);
+            this.lblOperaciones.Name = "lblOperaciones";
+            this.lblOperaciones.Size = new System.Drawing.Size(177, 264);
+            this.lblOperaciones.TabIndex = 7;
+            // 
+            // textNumero1
+            // 
+            this.textNumero1.Location = new System.Drawing.Point(46, 78);
+            this.textNumero1.Name = "textNumero1";
+            this.textNumero1.Size = new System.Drawing.Size(142, 27);
+            this.textNumero1.TabIndex = 8;
+            // 
+            // textNumero2
+            // 
+            this.textNumero2.Location = new System.Drawing.Point(390, 78);
+            this.textNumero2.Name = "textNumero2";
+            this.textNumero2.Size = new System.Drawing.Size(142, 27);
+            this.textNumero2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(787, 352);
+            this.Controls.Add(this.textNumero2);
+            this.Controls.Add(this.textNumero1);
+            this.Controls.Add(this.lblOperaciones);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cmbOperar);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button BtnCerrar;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+        private System.Windows.Forms.ComboBox cmbOperar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ListBox lblOperaciones;
+        private System.Windows.Forms.TextBox textNumero1;
+        private System.Windows.Forms.TextBox textNumero2;
+    }
+}
+
